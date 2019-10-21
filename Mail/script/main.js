2e2450d7270cc38fddf9c4220556cb6a1f51c7f5
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-21 15:10:23 +0200

    commenti

--- Mail/script/main.js
+++ Mail/script/main.js
@@ -3,8 +3,22 @@
 // messaggio appropriato
 
 // chiedo la mail (prompt)
+var askmail = prompt("Come ti chiami?");
+
 
 // creo una lista di 3 Mail
 
 // creo un calcolo che in base all imput restituisce un Output
 // de la mail match the 3 ok, sei invitato, altrimenti sei out
+
+var iscritti = ["Luca", "Marco", "Paolo"];
+iscritti.push(‘Michele’);
+
+if (askmail=Luca) {
+
+}
+
+// seleziono elemento html in cui fare output
+var htmlElement = document.getElementById("output");
+
+htmlElement.innerHTML = "Sei invitato!";
