88f72fab4412e52d59c8b7bf1d741ab6ade07a43
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-21 14:51:05 +0200

    comments

---
+++ Mail/script/main.js
@@ -0,0 +1,10 @@
+// Chiedi all’utente la sua email controlla che sia
+// nella lista di chi può accedere e stampa un
+// messaggio appropriato
+
+// chiedo la mail (prompt)
+
+// creo una lista di 3 Mail
+
+// creo un calcolo che in base all imput restituisce un Output
+// de la mail match the 3 ok, sei invitato, altrimenti sei out
