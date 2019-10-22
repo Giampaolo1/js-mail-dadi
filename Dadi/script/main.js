9d798f9495a95275c99dcf2b383f81088f315103
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-22 15:31:55 +0200

    s

--- Dadi/script/main.js
+++ Dadi/script/main.js
@@ -2,6 +2,7 @@
 
 // chiedere un numero all utente da 1 a 6 (prompt)
 var usernumber = prompt("Scegli un numero da 1 a 6");
+
 console.log(usernumber);
 
 // generare un math random da 1 a 6
@@ -9,6 +10,13 @@ var pcnumber = getRandomArbitrary(1, 6) {
   return Math.random() * (max - min) + min;
 }
 
+console.log(pcnumber);
+
 // stampare i risultati
 // confrontare i risultati
 // chi fa di piu' vince
+
+
+// function getRandomArbitrary(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
